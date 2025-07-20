fd910bd024b3b21c271c04762b9f43abe3023797
Vandivier <vandivi3r@gmail.com>
2025-07-19 21:06:07 -0400

    feat: rm orchestrator

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,7 +1,3 @@
 from dotenv import load_dotenv
 
 load_dotenv()
-
-from .orchestrator import root_agent
-
-__all__ = ["root_agent"]
--- backend/src/agent.py
+++ backend/src/agent.py
@@ -2,17 +2,19 @@ from google.adk.agents import Agent
 from src.utils import create_horse_fact, roll_a_dice
 from google.genai import types
 
+# TODO: RAG instance, or RAG as a tool?
+arias_tale_rag = create_horse_fact
 
-custom_tools_agent = Agent(
-    name="custom_tools_agent",
+root_agent = Agent(
+    name="arias_tale_rag_agent",
     model="gemini-2.0-flash",
     instruction=(
-        "You are a helpful research assistant that knows horse trivia. "
-        "For any user prompt mentioning a horse, use create_horse_fact tool. "
-        "For all other queries, use the roll_a_dice tool. "
+        "You are a helpful research assistant that knows Aria's Tale lore. "
+        "Roll a six-sided die if the user requests it."
+        "For other tasks, use the Aria's Tale RAG to bring in useful information, then think about the user's question and answer it."
     ),
     description="An agent that can answer questions.",
-    tools=[create_horse_fact, roll_a_dice],
+    tools=[arias_tale_rag, roll_a_dice],
     generate_content_config=types.GenerateContentConfig(
         temperature=0.0,
     ),
